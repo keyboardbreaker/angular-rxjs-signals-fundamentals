b5ab1b76044f1c78d4b76ec9e24ec7d22ad3f72a
yujin_tan <yujinost@gmail.com>
2025-02-12 02:42:02 +0000

    using computed count to update cartCount

--- apm-begin/src/app/cart/cart.service.ts
+++ apm-begin/src/app/cart/cart.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from "@angular/core";
+import { computed, effect, Injectable, signal } from "@angular/core";
+import { CartItem } from "./cart";
+import { Product } from "../products/product";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  cartItems = signal<CartItem[]>([]);
+  cartCount = computed(() => this.cartItems()
+  .reduce((accQty, item) => accQty + item.quantity, 0));
 
+  eLength = effect(() => console.log('Cart array length:', this.cartItems().length));
+
+  addToCart(product: Product): void {
+    //make a copy of passed in items and add new item.
+    this.cartItems.update(items => [...items, {product, quantity: 1 }]);
+  }
 }
--- apm-begin/src/app/products/product-detail/product-detail.component.ts
+++ apm-begin/src/app/products/product-detail/product-detail.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 
 import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
 import { Product } from '../product';
+import { Subscription } from 'rxjs';
+import { ProductService } from '../product.service';
+import { CartService } from 'src/app/cart/cart.service';
 
 @Component({
     selector: 'pm-product-detail',
@@ -9,10 +12,12 @@ import { Product } from '../product';
     standalone: true,
     imports: [NgIf, NgFor, CurrencyPipe]
 })
-export class ProductDetailComponent {
-  // Just enough here for the template to compile
+export class ProductDetailComponent implements OnChanges, OnDestroy {
   @Input() productId: number = 0;
   errorMessage = '';
+  sub!: Subscription;
+  private productService = inject(ProductService);
+  private cartService = inject(CartService);
 
   // Product to display
   product: Product | null = null;
@@ -20,6 +25,23 @@ export class ProductDetailComponent {
   // Set the page title
   pageTitle = this.product ? `Product Detail for: ${this.product.productName}` : 'Product Detail';
 
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const id = changes['productId'].currentValue;
+    this.sub = this.productService.getProduct(id).subscribe(
+      (product) => {
+        this.product = product;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    if(this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
+
   addToCart(product: Product) {
+    this.cartService.addToCart(product);
   }
 }
